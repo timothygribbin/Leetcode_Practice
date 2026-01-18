eace520cc06dfbd15ae4e8ccc8cd42602a11e8d9
timothygribbin <timothyjgribbin@gmail.com>
2026-01-17 20:00:49 -0500

    preOrderTraversal implementation

---
+++ BinaryTreePreOrderTraversal/Solution.java
@@ -0,0 +1,27 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Solution {
+        public List<Integer> preorderTraversal(TreeNode root) {
+            // Initialize list to hold traversal order
+            List<Integer> list = new ArrayList<>();
+
+            // Call the recursive method to traverse, we are passing the list by reference, so we can directly modify it and return the list after
+            preorderTraversalRecursive(root, list);
+            return list;   
+        }
+
+        public void preorderTraversalRecursive(TreeNode root, List<Integer> list) {
+            // Base case, if the root is null return
+            if(root == null) return;
+
+            // Add the root to the list first
+            list.add(root.val);
+
+            // Then call on the left node as the root
+            preorderTraversalRecursive(root.left, list);
+
+            /// Then call on the right node as the root
+            preorderTraversalRecursive(root.right, list);
+        }
+}
