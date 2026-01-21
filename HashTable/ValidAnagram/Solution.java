b356ef32668595ccdd0b1f7f8dc899dff5b68d5d
timothygribbin <timothyjgribbin@gmail.com>
2026-01-20 22:48:11 -0500

    ValidAnagram implementation

---
+++ HashTable/ValidAnagram/Solution.java
@@ -0,0 +1,35 @@
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        // If they are not the same length, they cannot contain the same letters
+        if(s.length() != t.length()) return false;
+
+        // Make a HashMap
+        HashMap<Character, Integer> map = new HashMap<>();
+
+        
+        // Build the map with s and it's number of occurences
+        for(int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+            map.put(c, map.getOrDefault(c, 0) + 1);
+        }
+
+        // Go through the same map with t
+        for(int i = 0; i < t.length(); i++) {
+            char c = t.charAt(i);
+
+            // If the map does not contain the key, then they are not anagrams
+            if(!map.containsKey(c)) return false;
+
+            // Change the input to - 1 if it does contain the key, think of this as marking it off as matching with s
+            map.put(c, map.get(c) - 1);
+
+            // If you have marked off all occurences, then remove it
+            if(map.get(c) == 0) {
+                map.remove(c);
+            }
+        }  
+
+        // Return if the map is empty or not
+        return map.isEmpty();
+    }
+}
